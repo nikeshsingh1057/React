4902d90b46fbe08e4aeb7d09ceac3d28f632614b
Nikesh Singh <nikeshsingh1057@gmail.com>
2024-02-12 23:43:59 +0530

    useState ke bare me _1

--- Lecture_5/components/Body.js
+++ Lecture_5/components/Body.js
@@ -1,17 +1,42 @@
+import { useState } from "react";
 import { restrautList } from "../config";
 import RestrauntCard from "./RestrauntCard";
 
 const Body = () => {
 
+  const [searchText, setSearchText] = useState("hello");
+
   return (
 
-    <div className="restaurant-list">
+    <>
+      <div>
 
-      {restrautList.map((restaurant) => {
-        return <RestrauntCard {...restaurant.data} key={restaurant.data.id} />;
-      })}
-      
-    </div>
+        <input
+          type="text"
+          className="search-input"
+          placeholder="Search"
+          value={searchText}
+          onChange={(e) => {
+           setSearchText(e.target.value);
+         }}
+        />
+
+      </div>
+
+      <h1>{searchText}</h1>
+      <button 
+        type="button">
+       
+       Search
+      </button>
+      <div className="restaurant-list">
+
+        {restrautList.map((restaurant) => {
+          return <RestrauntCard {...restaurant.data} key={restaurant.data.id} />;
+        })}
+
+      </div>
+    </>
   );
 
 };
